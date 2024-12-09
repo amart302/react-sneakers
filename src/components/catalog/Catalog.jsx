30164bf03e1f292a19f4cdcafc23bd9708417d68
Ahmed <amartazanov892@gmail.com>
2024-12-09 15:38:25 +0300

    commit 4

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -2,29 +2,46 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import "./catalog.css";
+import { useState } from "react";
 
 function Catalog(){
     const dispatch = useDispatch();
     const data = useSelector(state => state.data);
     const fav = useSelector(state => state.favourites);
-    
+    const [ favBlockStyles, setFavBlockStyles] = useState({
+        border: "solid 1px #f8f8f8",
+    })
+
+    function handleFavClick(item){
+        if(!fav.find(elem => elem == item)){
+            dispatch({
+                type: "ADD_TO_FAVOURITES",
+                payload: item
+            })
+            setFavBlockStyles({
+                border: "solid 1px #fef0f0",
+                backgroundColor: "#fef0f0"
+            })
+        }else{
+            dispatch({
+                type: "DELETE_TO_FAVOURITES",
+                payload: item
+            })
+            setFavBlockStyles({
+                border: "solid 1px #f8f8f8",
+                backgroundColor: "white"
+            })
+        }
+    }
+
     return(
         <div className="catalog_podBlock">
             {
                 data.map(item => (
                     <div className="productCard">
-                        <div className="favourites-block" onClick={() => {
-                            (!fav.find(elem => elem == item)) ? dispatch({
-                                type: "ADD_TO_FAVOURITES",
-                                payload: item
-                            }) : dispatch({
-                                type: "DELETE_TO_FAVOURITES",
-                                payload: item
-                            })
-                        }}>
+                        <div className="favourites-block" style={favBlockStyles} onClick={() => handleFavClick(item)}>
                             <img src={
-                                (fav.find(el => el == item)) ? "./images/favouritesIconActive.svg": "./images/favouritesIcon.svg"
-
+                                (fav.find(el => el == item)) ? "./images/favouritesIconActive.svg" : "./images/favouritesIcon.svg"
                             } alt="" />
                         </div>
                         <img src={item.imgPath} alt="" />
